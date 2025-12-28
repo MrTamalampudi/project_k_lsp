adeb416af1e4ea50542d9faf82fea468f57945c5
ManikantaReddy <manikantareddytamalampudi7@gmail.com>
2025-12-28 17:00:15 +0530

    rough 1

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,8 @@
-use parking_lot::RwLock;
+use dashmap::DashMap;
 use parser::parser::Parser;
 use parser::token::Token;
 use parser::{source_code_to_lexer, CompilationContext};
+use span::{Location, Span};
 use std::collections::HashMap;
 use std::env;
 use std::path::PathBuf;
@@ -12,12 +13,15 @@ use tower_lsp::jsonrpc::Result as LspResult;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+use crate::semantic_tokens::{update_semantic_tokens, SEMANTIC_TOKEN_TYPES};
+
 mod completion;
 mod semantic_tokens;
 
 pub struct Backend {
     client: Client,
-    token_map: RwLock<HashMap<Uri, Vec<Token>>>,
+    token_map: DashMap<Uri, Vec<Token>>,
+    semantic_tokens_map: DashMap<Uri, Vec<SemanticToken>>,
 }
 
 impl Backend {
@@ -28,8 +32,7 @@ impl Backend {
             .await;
         let mut ctx = CompilationContext::new(PathBuf::from(uri.path().as_str()));
         let mut lexer = source_code_to_lexer(text.clone(), &mut ctx);
-        let mut lock = self.token_map.write();
-        lock.insert(uri, lexer.tokens.clone());
+        self.token_map.insert(uri, lexer.tokens.clone());
         Parser::new(&mut lexer, &mut ctx).parse();
         let mut diagnostic_map: HashMap<Uri, Vec<Diagnostic>> = HashMap::new();
         //converting compiler errors to lsp errors and
@@ -71,24 +74,49 @@ impl LanguageServer for Backend {
     async fn initialize(&self, params: InitializeParams) -> LspResult<InitializeResult> {
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
-                definition_provider: Some(OneOf::Right(DefinitionOptions {
-                    work_done_progress_options: WorkDoneProgressOptions {
-                        work_done_progress: None,
-                    },
-                })),
-                code_lens_provider: Some(CodeLensOptions {
-                    resolve_provider: Some(true),
-                }),
-                completion_provider: Some(CompletionOptions {
-                    resolve_provider: Some(false),
-                    trigger_characters: Some(vec![".".to_string(), "#".to_string()]),
-                    work_done_progress_options: Default::default(),
-                    all_commit_characters: None,
-                    completion_item: None,
-                }),
+                // definition_provider: Some(OneOf::Right(DefinitionOptions {
+                //     work_done_progress_options: WorkDoneProgressOptions {
+                //         work_done_progress: None,
+                //     },
+                // })),
+                // code_lens_provider: Some(CodeLensOptions {
+                //     resolve_provider: Some(true),
+                // }),
+                // completion_provider: Some(CompletionOptions {
+                //     resolve_provider: Some(false),
+                //     trigger_characters: Some(vec![" ".to_string()]),
+                //     work_done_progress_options: Default::default(),
+                //     all_commit_characters: None,
+                //     completion_item: None,
+                // }),
                 text_document_sync: Some(TextDocumentSyncCapability::Kind(
                     TextDocumentSyncKind::FULL,
                 )),
+                semantic_tokens_provider: Some(
+                    SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+                        SemanticTokensRegistrationOptions {
+                            text_document_registration_options: {
+                                TextDocumentRegistrationOptions {
+                                    document_selector: Some(vec![DocumentFilter {
+                                        language: Some("project_k".to_string()),
+                                        scheme: Some("file".to_string()),
+                                        pattern: Some("*.ll".to_string()),
+                                    }]),
+                                }
+                            },
+                            semantic_tokens_options: SemanticTokensOptions {
+                                work_done_progress_options: WorkDoneProgressOptions::default(),
+                                legend: SemanticTokensLegend {
+                                    token_types: SEMANTIC_TOKEN_TYPES.to_vec(),
+                                    token_modifiers: vec![],
+                                },
+                                range: Some(false),
+                                full: Some(SemanticTokensFullOptions::Bool(true)),
+                            },
+                            static_registration_options: StaticRegistrationOptions::default(),
+                        },
+                    ),
+                ),
                 ..Default::default()
             },
             ..Default::default()
@@ -120,6 +148,23 @@ impl LanguageServer for Backend {
             )
             .await
     }
+    async fn semantic_tokens_full(
+        &self,
+        params: SemanticTokensParams,
+        _: CancellationToken,
+    ) -> Result<Option<SemanticTokensResult>> {
+        let tokens = self.token_map.get(&params.text_document.uri).unwrap();
+        let st = update_semantic_tokens(&tokens);
+
+        self.client
+            .show_message(MessageType::INFO, format!("{:#?}", st))
+            .await;
+
+        Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
+            result_id: None,
+            data: st,
+        })))
+    }
 
     async fn code_lens(
         &self,
@@ -147,26 +192,6 @@ impl LanguageServer for Backend {
 
         Ok(Some(vec![code_lens.clone(), code_lens.clone()]))
     }
-
-    // async fn completion(
-    //     &self,
-    //     params: CompletionParams,
-    //     token: CancellationToken,
-    // ) -> Result<Option<CompletionResponse>> {
-    //     let tokens = self.token_map.read();
-    //     let tokens = tokens
-    //         .get(&params.text_document_position.text_document.uri)
-    //         .unwrap();
-    //     // self.client
-    //     //     .show_message(MessageType::INFO, format!("tokendds {:#?}", tokens))
-    //     //     .await;
-    //     let t = IntelliSense::new(tokens, &params.text_document_position.position).complete();
-    //     Ok(Some(CompletionResponse::Array(t)))
-    //     // self.client
-    //     //     .show_message(MessageType::INFO, format!("tokens {:#?}", token))
-    //     //     .await;
-    //     //Ok(None)
-    // }
 }
 
 #[tokio::main]
@@ -176,9 +201,14 @@ async fn main() {
 
     let (service, socket, pending) = LspService::new(|client| Backend {
         client,
-        token_map: RwLock::new(HashMap::new()),
+        token_map: DashMap::new(),
+        semantic_tokens_map: DashMap::new(),
     });
     Server::new(stdin, stdout, socket, pending)
         .serve(service)
         .await;
+}
+
+pub fn to_pos(span: Span) -> (u32, u32) {
+    ((span.start.line - 1) as u32, (span.start.column - 1) as u32)
 }
